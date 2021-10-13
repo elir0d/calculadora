3040b712b293a7891b068be5d0ceb80d5e0b8ecf
elir0d <49011043+elir0d@users.noreply.github.com>
2021-10-13 17:18:07 -0300

    refactor: :recycle: Change paramenters name
    Change parameters name for better readability

--- interface.py
+++ interface.py
@@ -2,5 +2,5 @@ from abc import ABC, abstractmethod, abstractclassmethod
 class ICalc(ABC):
     '''Interface responsável por implementar as classes/metodos concretos'''
     @abstractmethod
-    def calcular(self, n1, n2):
+    def calcular(self, numero1: int, numero2: int) -> int:
         pass
