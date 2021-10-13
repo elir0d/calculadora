0f318442bd9c85829071382cd663b5f3785b80da
elir0d <49011043+elir0d@users.noreply.github.com>
2021-10-13 17:21:33 -0300

    test: :white_check_mark: Add tests

---
+++ teste_operacoes.py
@@ -0,0 +1,35 @@
+import calculadora as calc
+import contexto as cont
+import unittest
+
+
+
+class Testes(unittest.TestCase):
+    def test_deveria_somar_dois_numeros_e_retornar_4(self):
+        operacao = cont.operacao['+']
+        self.assertEqual(calc.executaCalculo.realiza_calculo(self, 2, 2, operacao), 4)
+
+    def test_deveria_subtrair_2_de_4_e_retornar_2(self):
+        operacao = cont.operacao['-']
+        self.assertEqual(calc.executaCalculo.realiza_calculo(self, 4, 2, operacao), 2)
+
+    def test_deveria_multiplicar_dois_numeros_e_retornar_6(self):
+        operacao = cont.operacao['*']
+        self.assertEqual(calc.executaCalculo.realiza_calculo(self, 2, 3, operacao), 6)
+
+    def test_deveria_dividir_dois_numeros_e_retornar_2(self):
+        operacao = cont.operacao['/']
+        self.assertEqual(calc.executaCalculo.realiza_calculo(self, 10, 5, operacao), 2)
+
+    def test_deveria_retornar_None_para_divisoes_por_0(self):
+        operacao = cont.operacao['/']
+        self.assertEqual(calc.executaCalculo.realiza_calculo(self, 0, 0, operacao), None)
+        self.assertEqual(calc.executaCalculo.realiza_calculo(self, 2, 0, operacao), None)
+
+def runTests():
+    suite = unittest.defaultTestLoader.loadTestsFromTestCase(Testes)
+    unittest.TextTestRunner(verbosity=2,failfast=False).run(suite)
+
+if __name__ == '__main__':
+    runTests()
+
