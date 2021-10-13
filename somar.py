61a5faab3e8ee0cb9a2d8b9a4aa62f91f89c2bdf
elir0d <49011043+elir0d@users.noreply.github.com>
2021-10-13 17:20:59 -0300

    refactor: :recycle: change parameters names
    Change parameters name for better readability

--- somar.py
+++ somar.py
@@ -2,5 +2,5 @@ from interface import ICalc
 
 '''Classe concreta'''
 class Somar(ICalc):
-    def calcular(self, numero1, numero2):
+    def calcular(self, numero1: int, numero2: int) -> int:
         return numero1 + numero2
