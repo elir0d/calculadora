88055ef837cfb7eef2751fc632dcd3760a616e4d
elir0d <49011043+elir0d@users.noreply.github.com>
2021-10-13 17:17:05 -0300

    feat: :sparkles: Add a try -> except case
    Create a ZeroDivisionError except case to handle division by sero

--- dividir.py
+++ dividir.py
@@ -2,5 +2,8 @@ from interface import ICalc
 
 '''Classe concreta'''
 class Dividir():
-    def calcular(self, numero1, numero2):
-        return numero1 / numero2
+    def calcular(self, numero1: int, numero2: int) -> int:
+        try:
+          return numero1 / numero2
+        except ZeroDivisionError:
+          return None
