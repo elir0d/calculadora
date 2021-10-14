9ad0771b42e5841bf23c9afa3be1e15dddea93a7
elir0d <49011043+elir0d@users.noreply.github.com>
2021-10-14 00:36:17 -0300

    fix: :bug: Fix test imports and dependencies

--- teste_operacoes.py
+++ teste_operacoes.py
@@ -1,26 +1,27 @@
 import unittest
-import contexto as cont
+import FabricaDeOperador as fab
 import calculadora as calc
 
+ef = calc.executaFabrica()
 class Testes(unittest.TestCase):
     def test_deveria_somar_dois_numeros_e_retornar_4(self):
-        operacao = cont.operacao['+']
+        operacao = ef.fabrica_operador('+')
         self.assertEqual(calc.executaCalculo.realiza_calculo(self, 2, 2, operacao), 4)
 
     def test_deveria_subtrair_2_de_4_e_retornar_2(self):
-        operacao = cont.operacao['-']
+        operacao = ef.fabrica_operador('-')
         self.assertEqual(calc.executaCalculo.realiza_calculo(self, 4, 2, operacao), 2)
 
     def test_deveria_multiplicar_dois_numeros_e_retornar_6(self):
-        operacao = cont.operacao['*']
+        operacao = ef.fabrica_operador('*')
         self.assertEqual(calc.executaCalculo.realiza_calculo(self, 2, 3, operacao), 6)
 
     def test_deveria_dividir_dois_numeros_e_retornar_2(self):
-        operacao = cont.operacao['/']
+        operacao = ef.fabrica_operador('/')
         self.assertEqual(calc.executaCalculo.realiza_calculo(self, 10, 5, operacao), 2)
 
     def test_deveria_retornar_None_para_divisoes_por_0(self):
-        operacao = cont.operacao['/']
+        operacao = ef.fabrica_operador('/')
         self.assertEqual(calc.executaCalculo.realiza_calculo(self, 0, 0, operacao), None)
         self.assertEqual(calc.executaCalculo.realiza_calculo(self, 2, 0, operacao), None)
 
