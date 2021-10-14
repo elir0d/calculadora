f49fa20701ef656cbab227461df81469696aca89
Rodrigo Trindade <49011043+elir0d@users.noreply.github.com>
2021-10-14 01:07:43 -0300

    Update teste_operacoes.py

--- teste_operacoes.py
+++ teste_operacoes.py
@@ -4,21 +4,21 @@ import calculadora as calc
 
 ef = calc.executaFabrica()
 class Testes(unittest.TestCase):
-    def test_deveria_somar_dois_numeros_e_retornar_4(self):
+    def test_deveria_somar_dois_numeros_e_retornar_10(self):
         operacao = ef.fabrica_operador('+')
-        self.assertEqual(calc.executaCalculo.realiza_calculo(self, 2, 2, operacao), 4)
+        self.assertEqual(calc.executaCalculo.realiza_calculo(self, 5, 5, operacao), 10)
 
-    def test_deveria_subtrair_2_de_4_e_retornar_2(self):
+    def test_deveria_subtrair_3_de_6_e_retornar_3(self):
         operacao = ef.fabrica_operador('-')
-        self.assertEqual(calc.executaCalculo.realiza_calculo(self, 4, 2, operacao), 2)
+        self.assertEqual(calc.executaCalculo.realiza_calculo(self, 6, 3, operacao), 3)
 
-    def test_deveria_multiplicar_dois_numeros_e_retornar_6(self):
+    def test_deveria_multiplicar_dois_numeros_e_retornar_50(self):
         operacao = ef.fabrica_operador('*')
-        self.assertEqual(calc.executaCalculo.realiza_calculo(self, 2, 3, operacao), 6)
+        self.assertEqual(calc.executaCalculo.realiza_calculo(self, 10, 5, operacao), 50)
 
-    def test_deveria_dividir_dois_numeros_e_retornar_2(self):
+    def test_deveria_dividir_dois_numeros_e_retornar_5(self):
         operacao = ef.fabrica_operador('/')
-        self.assertEqual(calc.executaCalculo.realiza_calculo(self, 10, 5, operacao), 2)
+        self.assertEqual(calc.executaCalculo.realiza_calculo(self, 25, 5, operacao), 5)
 
     def test_deveria_retornar_None_para_divisoes_por_0(self):
         operacao = ef.fabrica_operador('/')
